0864ad3e97288e9ad35c0aa2f51b4150e4532409
XUENN\blackie.tsai <blackie1019@gmail.com>
2018-01-02 02:06:34 +0800

    Updated

---
+++ index.js
@@ -0,0 +1,21 @@
+var redis = require("redis");
+
+var client = redis.createClient(6379, 'ironman.zsf7la.ng.0001.apne1.cache.amazonaws.com', {no_ready_check: true});
+
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
